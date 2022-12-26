84e4b05afda262244f9951c868e9f6579b377377
TheConnMan <bcconn2112@gmail.com>
2022-12-26 11:11:25 -0500

    Support sub-queries for other RSS fields

--- api/index.js
+++ api/index.js
@@ -6,25 +6,32 @@ import log4js from 'log4js';
 var logger = log4js.getLogger();
 
 export async function api(req, res) {
+  const page = req.query.page;
+  const elementQuery = req.query.query;
+  const titleQuery = req.query.titleQuery;
+  const descriptionQuery = req.query.descriptionQuery;
+  const dateQuery = req.query.dateQuery;
   try {
-    const response = await fetch(req.query.page);
+    const response = await fetch(page);
     const html = await response.text();
-    const parsed = parse(html).querySelectorAll(req.query.query);
+    const parsed = parse(html).querySelectorAll(elementQuery);
     res.setHeader('Content-Type', 'text/xml');
-    res.send(formatRSS(req.query.page, parsed));
+    res.send(formatRSS(page, parsed, titleQuery, descriptionQuery, dateQuery));
   } catch (e) {
     logger.error(e);
     res.status(500).send(e.message);
   }
 }
 
-function formatRSS(page, elements) {
+function formatRSS(page, elements, titleQuery, descriptionQuery, dateQuery) {
   var feed = new RSS({
     title: `${page} Elements`
   });
   elements.forEach(element => {
     feed.item({
-      title: element.innerText,
+      title: (titleQuery ? element.querySelector(titleQuery) : element).innerText,
+      description: descriptionQuery ? element.querySelector(descriptionQuery).innerText : null,
+      date: dateQuery ? element.querySelector(dateQuery).innerText : null,
       url: page
     });
   });
